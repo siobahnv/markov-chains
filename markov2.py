290bd27f327469622eacbcf5dce51ba998cb1079
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-04 19:58:22 -0700

    Add working markov2 file and punctuation file

--- markov2.py
+++ markov2.py
@@ -5,7 +5,6 @@ import sys
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
-
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
@@ -15,27 +14,19 @@ def open_and_read_file(file_path):
     return open(file_path).read()
 
 
-def make_chains(text_string, size_n_grams):
+def make_chains(text_string, n_grams):
     """Take input text as string; return dictionary of Markov chains.
-
     A chain will be a key that consists of a tuple of (word1, word2)
     and the value would be a list of the word(s) that follow those two
     words in the input text.
-
     For example:
-
         >>> chains = make_chains("hi there mary hi there juanita")
-
     Each bigram (except the last) will be a key in chains:
-
         >>> sorted(chains.keys())
         [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
-
     Each item in chains is a list of all possible following words:
-
         >>> chains[('hi', 'there')]
         ['mary', 'juanita']
-        
         >>> chains[('there','juanita')]
         [None]
     """
@@ -46,67 +37,57 @@ def make_chains(text_string, size_n_grams):
 
     split_string = text_string.split()
     split_string.append(" ")
+    counter = 0
 
-
-    for index in range(len(split_string) - int(size_n_grams)):
-        print(index)
-        value = []
+    while counter < (len(split_string) - n_grams):
+        print(counter)
         key = []
+        value = []
 
-
-        for n in range(int(size_n_grams)):
-            new_count = (index) + n
+        for n in range(n_grams):
+            new_count = (counter) + n
             (key.append(split_string[new_count]))
-        # for n in range(int(size_n_grams)):
-        #     key.append(split_string[index + n])
         
-        # value = split_string[index + int(size_n_grams)]
-        value = split_string[new_count + 1]
         key = tuple(key)
+        value = split_string[new_count + 1]
 
-        if key in chains:
-            chains[key].append(value)
-        else:
+        if key not in chains:
             chains[key] = [value]
-        
+        else:
+            chains[key].append(value)
+
+        counter += 1
+    print(chains)
     return chains
 
+def make_text(chains, n_grams):
 
-def make_text(chains, size_n_grams):
-    """Return text from chains."""
-    key = choice(list(chains))   
-    print(key)
-
+    key = choice(list(chains))
     word_list = list(key)
-    print (word_list)
-
     word_value = choice(chains[key])
 
     while word_value is not " ":
-        
         key = (key[1::])
         key = list(key)
         key.append(word_value)
         key = tuple(key)
         word_list.append(word_value)
         word_value = choice(chains[key])
-   
 
-    
+
     return " ".join(word_list)
 
 
-# input_path = "green-eggs.txt"
-input_path = sys.argv[1]
-input_n_grams = sys.argv[2]
 
+
+input_path = sys.argv[1]
+n_grams = int(sys.argv[2])
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text, input_n_grams)
-
+chains = make_chains(input_text, n_grams)
 # Produce random text
-random_text = make_text(chains, input_n_grams)
+random_text = make_text(chains, n_grams)
 
 print(random_text)
--- markov_punctuation.py
+++ markov_punctuation.py
@@ -1,11 +1,11 @@
 """Generate Markov text from text files."""
 
 from random import choice
+import random
 import sys
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
-
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
@@ -15,98 +15,90 @@ def open_and_read_file(file_path):
     return open(file_path).read()
 
 
-def make_chains(text_string, size_n_grams):
+def make_chains(text_string):
     """Take input text as string; return dictionary of Markov chains.
-
     A chain will be a key that consists of a tuple of (word1, word2)
     and the value would be a list of the word(s) that follow those two
     words in the input text.
-
     For example:
-
         >>> chains = make_chains("hi there mary hi there juanita")
-
     Each bigram (except the last) will be a key in chains:
-
         >>> sorted(chains.keys())
         [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
-
     Each item in chains is a list of all possible following words:
-
         >>> chains[('hi', 'there')]
         ['mary', 'juanita']
-        
         >>> chains[('there','juanita')]
         [None]
     """
-
     chains = {}
-
-    # your code goes here
-
+    counter = 0
     split_string = text_string.split()
     split_string.append(" ")
 
-
-    for index in range(len(split_string) - int(size_n_grams)):
-        print(index)
-        value = []
-        key = []
-
-
-        for n in range(int(size_n_grams)):
-            new_count = (index) + n
-            (key.append(split_string[new_count]))
-        # for n in range(int(size_n_grams)):
-        #     key.append(split_string[index + n])
-        
-        # value = split_string[index + int(size_n_grams)]
-        value = split_string[new_count + 1]
-        key = tuple(key)
-
-        if key in chains:
-            chains[key].append(value)
-        else:
+    while counter < (len(split_string) - 2):
+        key = (split_string[counter], split_string[counter+1])
+        value = split_string[counter + 2]
+        if key not in chains:
             chains[key] = [value]
-        
+
+        else:
+            chains[key].append(value)
+
+        counter += 1
+
+    print(chains)
     return chains
 
+    # # your code goes here
 
-def make_text(chains, size_n_grams):
-    """Return text from chains."""
-    key = choice(list(chains))   
-    print(key)
+def get_starting_key(chains):
+    print("Getting starting key")
+    for key in chains.keys():
+        key = random.choice(list(chains))
+        print(key)  
+        if key[0][0].isupper():
+            starting_key = key
+            return starting_key
+       
 
-    word_list = list(key)
-    print (word_list)
 
+
+def make_text(chains, starting_key):
+    print("making text")
+    key = starting_key
     word_value = choice(chains[key])
+    punctuation = [".", "!", "?"]
+    word_list = list(starting_key)
+    # print("starting key", word_list, "value",word_value)
 
     while word_value is not " ":
-        
-        key = (key[1::])
-        key = list(key)
-        key.append(word_value)
-        key = tuple(key)
-        word_list.append(word_value)
-        word_value = choice(chains[key])
-   
+            key = (key[1], word_value)
+            # if word_value[0].islower():
+            word_list.append(word_value)
+            if word_value[-1] in punctuation:
+                break
 
-    
+            word_value = choice(chains[key])
+
     return " ".join(word_list)
 
 
-# input_path = "green-eggs.txt"
+
+
+            # counter += 1
+    # print(chains)
+
+
+
+
 input_path = sys.argv[1]
-input_n_grams = sys.argv[2]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
-# Get a Markov chain
-chains = make_chains(input_text, input_n_grams)
-
-# Produce random text
-random_text = make_text(chains, input_n_grams)
-
+chains = make_chains(input_text)
+starting_key = get_starting_key(chains)
+random_text = make_text(chains, starting_key)
+#
 print(random_text)
